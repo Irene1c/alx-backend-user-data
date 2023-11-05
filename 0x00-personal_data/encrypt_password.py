e4130ff9171d916cb4f9b7127d023e8bf64f4d28
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-05 19:41:19 +0000

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,8 +8,20 @@ def hash_password(password: str) -> bytes:
         returns a salted, hashed password
     """
 
-    password = password.encode('utf-8')  # encode to bytes
+    psswd = password.encode('utf-8')  # encode to bytes
     salt = bcrypt.gensalt()
-    hashed_passwd = bcrypt.hashpw(password, salt)
+    hashed_passwd = bcrypt.hashpw(psswd, salt)
 
     return hashed_passwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Function that validates that the provided password
+        matches the hashed password
+    """
+
+    passwd = password.encode('utf-8')
+    if bcrypt.checkpw(passwd, hashed_password):
+        return True
+    else:
+        return False
