5fb7bcd15f41ef7d8521f3450d0f7750f2a90eca
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-08 19:06:53 +0000

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic auth module """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -19,3 +21,18 @@ class BasicAuth(Auth):
 
         en_string = authorization_header.split(' ')
         return en_string[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Function that returns the decoded value of a Base64 string"""
+
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            d_str = base64.b64decode(
+                    base64_authorization_header, validate=True)
+            return d_str.decode('utf-8')
+        except (binascii.Error, TypeError):
+            return None
