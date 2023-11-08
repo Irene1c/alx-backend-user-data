dfd9f33cbe7feee57d4ca2dc524138f181ff3350
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-08 18:31:25 +0000

    Basic - Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic auth class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ base64 encoding """
+
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        en_string = authorization_header.split(' ')
+        return en_string[1]
