f66a19b31b168d04474c6c01f090ab0600cf48f7
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-11-20 14:35:28 +0000

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,19 @@ def logout():
     return redirect(url_for('basic'))
 
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    """User profile"""
+
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+    if user is not None:
+        return jsonify({"email": f"{user.email}"}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
